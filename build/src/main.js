3cab525116085ac6c688665848f42f7f610b0303
Senthil Maruthaiappan <senips@gmail.com>
2019-10-15 16:01:06 -0400

    clean it

--- build/src/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function main() {
-    console.log('hello main');
-}
-exports.main = main;
-//# sourceMappingURL=main.js.map
